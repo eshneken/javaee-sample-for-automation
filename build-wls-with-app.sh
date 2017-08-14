206b68ca1bec4dcab6aa4628d459d0f17254b994
Ed Shnekendorf <eshneken@gmail.com>
2017-08-14 15:55:22 -0400

    elevate privileges for docker

--- build-wls-with-app.sh
+++ build-wls-with-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build the WLS sample app
-docker build -t 129.213.60.3:5000/wls_sample_app .
+sudo docker build -t 129.213.60.3:5000/wls_sample_app .
 
 # push the app to local docker repo
-docker push 129.213.60.3:5000/wls_sample_app
+sudo docker push 129.213.60.3:5000/wls_sample_app
