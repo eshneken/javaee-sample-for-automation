50aa9f0531d9e0a5e39864d5b3ef6919364d2b5d
Ed Shnekendorf <eshneken@gmail.com>
2018-03-06 12:10:13 -0500

    update docker server

--- build-wls-with-app.sh
+++ build-wls-with-app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build the WLS sample app
-sudo docker build -t 129.213.60.3:5000/wls_sample_app .
+sudo docker build -t 129.213.22.38:5000/wls_sample_app .
 
 # push the app to local docker repo
-sudo docker push 129.213.60.3:5000/wls_sample_app
+sudo docker push 129.213.22.38:5000/wls_sample_app
